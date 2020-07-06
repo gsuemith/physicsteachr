3a3559e1efb495c8fb70de0dbb0a45d487374e59
Garrick Suemith <garrick.suemith@gmail.com>
2020-07-06 18:18:26 -0400

    fixed active shading

---
+++ run.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, url_for
+
+app = Flask(__name__)
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template('home.html')
+
+@app.route("/about")
+def about():
+    return render_template('about.html')
+
+@app.route("/products")
+def products():
+    return render_template('products.html')
+
+@app.route("/store")
+def store():
+    return render_template('store.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
